673f47b4eb4906b6dd40497dae185414d2e9d48e
homedew <homedew.tc@gmail.com>
2024-03-09 17:31:46 +0700

    Add Project

---
+++ src/main/java/com/io/mango/mangoproject/MangoprojectApplication.java
@@ -0,0 +1,13 @@
+package com.io.mango.mangoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangoprojectApplication.class, args);
+	}
+
+}
